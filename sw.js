536ac08cc0b3b217d5a90591552ff6c8e9aa2744
ad546 <ad546github@gmail.com>
2018-08-02 10:12:25 -0500

    accepted project

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'v2';
+var cacheName = 'restaurant';
 var cacheFiles = [
     '/',
     'index.html',
@@ -25,7 +25,7 @@ self.addEventListener('install', function(event) {
     
     event.waitUntil(
         caches.open(cacheName).then(function(cache) {
-            console.log("Caching cacheFiles");
+            console.log("Caching");
             return cache.addAll(cacheFiles);
         })
     )
